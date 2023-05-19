75dc54ebb7707f8db9e75c0238b98aac9ac83ffe
Dicte Waahlstrand <dicte2002@gmail.com>
2023-05-19 11:38:37 +0200

    DML

---
+++ DML-script.sql
@@ -0,0 +1,14 @@
+INSERT INTO patient 
+VALUES ('1908765103','Peter','Petersen','Østre Voldgade','33 3tv','2400','50343434','NULL','760819','48');
+INSERT INTO patient
+VALUES ('1805026783','Marcus','Rasmussen','Edward Thomsensvej','89 7th','2300','81740306','mnsr@hotmail.dk','020518','21');
+INSERT INTO patient
+VALUES ('1411025134','Dicte','Wåhlstrand','Morbærhaven','7L','2620','31489945','dicte2002@gmail.com','021114','20');
+
+INSERT INTO doctor
+VALUES ('17352','300689617','Jens','Jensen','generel practice','71898903','jeje@lægehusetkøbenhavn.dk');
+INSERT INTO doctor
+VALUES ('17625','282099252','Line','Andersen','generel practice','71847603','lian@lægehusetkøbenhavn.dk');
+
+
+
